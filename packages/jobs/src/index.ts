79923b4b256fea8cb7916e023607ddde299714e6
Maksim <kodermax@gmail.com>
2025-11-29 22:21:20 +0300

    fix

--- packages/jobs/src/index.ts
+++ packages/jobs/src/index.ts
@@ -4,7 +4,6 @@ export {
   inngest,
   inngestFunctions,
   parseNewResumesChannel,
-  refreshVacancyResponsesChannel,
   refreshVacancyResponsesFunction,
   screenNewResponsesChannel,
   screenResponseFunction,
